7026b914e15b2499bcb18af1327b20fb70f0e317
卢俊伟 <lujunwei@lujunweideMacBook-Pro.local>
2017-01-05 15:19:51 +0800

    增加弹窗时，不能滑动方法

--- src/views/MessageDetailView.vue
+++ src/views/MessageDetailView.vue
@@ -17,52 +17,53 @@
       <div class="message-body">今天打算投多少？今天打算投多少？今天打算投多少？今天打算投多少？</div>
       <img class="avatar" src="../images/icon-avatar.png" />
     </div>
-     <div class="right dialog-item">
+    <div class="right dialog-item">
       <div class="message-body">今天打算投多少？今天打算投多少？今天打算投多少？今天打算投多少？</div>
       <img class="avatar" src="../images/icon-avatar.png" />
     </div>
-     <div class="right dialog-item">
+    <div class="right dialog-item">
       <div class="message-body">今天打算投多少？今天打算投多少？今天打算投多少？今天打算投多少？</div>
       <img class="avatar" src="../images/icon-avatar.png" />
     </div>
-     <div class="right dialog-item">
+    <div class="right dialog-item">
       <div class="message-body">今天打算投多少？今天打算投多少？今天打算投多少？今天打算投多少？</div>
       <img class="avatar" src="../images/icon-avatar.png" />
     </div>
-     <div class="left dialog-item">
+    <div class="left dialog-item">
       <img class="avatar" src="../images/icon-avatar.png" />
       <div class="message-body">今天打算投多少？今天打算投多少？今天打算投多少？今天打算投多少？</div>
     </div>
-     <div class="right dialog-item">
+    <div class="right dialog-item">
       <div class="message-body">今天打算投多少？今天打算投多少？今天打算投多少？今天打算投多少？</div>
       <img class="avatar" src="../images/icon-avatar.png" />
     </div>
-     <div class="right dialog-item">
+    <div class="right dialog-item">
       <div class="message-body">今天打算投多少？今天打算投多少？今天打算投多少？今天打算投多少？</div>
       <img class="avatar" src="../images/icon-avatar.png" />
     </div>
-     <div class="right dialog-item">
+    <div class="right dialog-item">
       <div class="message-body">今天打算投多少？今天打算投多少？今天打算投多少？今天打算投多少？</div>
       <img class="avatar" src="../images/icon-avatar.png" />
     </div>
-     <div class="right dialog-item">
+    <div class="right dialog-item">
       <div class="message-body">今天打算投多少？今天打算投多少？今天打算投多少？今天打算投多少？</div>
       <img class="avatar" src="../images/icon-avatar.png" />
     </div>
 
    </div>
-    <div class="btn-message" @click="onMessage">发消息</div>
-    <div class="message-dialog" v-show="isShowDialog">
-      <div class="masker" v-show="isShowDialog"></div>
-      <div class="dialog-content">
-        <div class="dialog-title">回复</div>
-        <textarea class="reply-content" placeholder="请输入…"></textarea>
-        <div class="btn-wrapper">
-          <div class="btn-cancel" @click="onCancel">取消</div>
-          <div class="btn-send" @click="onSend">发送</div>
-        </div>
+  <div class="btn-message" @click="onMessage">发消息</div>
+  </div>
+  <div class="message-dialog" v-show="isShowDialog">
+    <div class="masker" v-show="isShowDialog"></div>
+    <div class="dialog-content">
+      <div class="dialog-title">回复</div>
+      <textarea class="reply-content" placeholder="请输入…"></textarea>
+      <div class="btn-wrapper">
+        <div class="btn-cancel" @click="onCancel">取消</div>
+        <div class="btn-send" @click="onSend">发送</div>
       </div>
     </div>
+  </div>
   </div>
 </template>
 
@@ -86,14 +87,17 @@ export default {
   methods: {
     onMessage() {
       this.isShowDialog = true;
+      Util.disableScrolling();
     },
     onCancel() {
       this.isShowDialog = false;
+      Util.enableScrolling();
     },
     onSend() {
       // TODO:展示，需要删除
       Toast("发送消息");
       this.isShowDialog = false;
+      Util.enableScrolling();
     }
   },
   beforeMount () {
