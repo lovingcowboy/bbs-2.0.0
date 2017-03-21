9aa1a0fd6f0cebd09ade3581df7b019798050410
卢俊伟 <lujunwei@lujunweideMacBook-Pro.local>
2017-03-21 16:21:09 +0800

    压缩图片

--- src/services/openapi.js
+++ src/services/openapi.js
@@ -1,7 +1,10 @@
 let openapi = {}
 // let domain = window.location.protocol + '//192.168.8.188:7010' // dev
 // let domain = window.location.protocol + '//10.100.1.39:80'
-let domain = '//10.100.11.110:9500'
+// let domain = '//10.100.11.110:9500'
+let domain = '//bbs.tuandai.com'
+// let domain = '//dev.tuandai.com'	//dev
+// let domain = '//bbs5.tuandai.com'	//bbs5
 // let domain = '//bbs2.tuandai.com'
 
 Object.defineProperty(openapi, 'domain', {
